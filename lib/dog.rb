da9a325183d0ccb53c9f7707be59b0293228b2ee
erikn775 <erikn775@gmail.com>
2020-11-18 14:23:19 -0700

    commit tree 2074ff2f049e2cc4923213dd02418b3411d9d82c

---
+++ lib/dog.rb
@@ -0,0 +1,73 @@
+class Dog
+    attr_accessor :name, :breed
+    attr_reader :id
+
+    def initialize(id: nil, name:, breed:)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        sql = <<-SQL
+        CREATE TABLE dogs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        )
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = <<-SQL
+        DROP TABLE dogs
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+    def save
+        if self.id
+            self.update
+        else
+        sql = <<-SQL
+        INSERT INTO dogs (name, breed) VALUES (?,?)
+        SQL
+
+        DB[:conn].execute(sql, name, breed)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")
+        end
+    end
+
+    def self.create(name:, breed:)
+        dog = Dog.new(@name, @breed)
+        dog.save
+        dog
+    end
+
+    def self.new_from_db(row)
+        new_dog = self.new
+        new_dog.id = row[0]
+        new_dog.name = row[1]
+        new_dog.breed = row[2]
+        new_dog
+    end
+
+    def self.find_by_name(name)
+        sql = <<-SQL
+        SELECT * FROM dogs WHERE id = ?
+        LIMIT 1
+        SQL
+
+        DB[:conn].execute(sql, name).map do |row|
+            self.new_from_db(row)
+        end
+        
+
+    end
+
+    def update
+        sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+end
